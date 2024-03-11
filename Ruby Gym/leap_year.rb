40002fce40cf8d0ca1288bc5a7302b39d1bc6dc8
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-11 20:55:21 +0000

    created ruby_year.rb file

---
+++ Ruby Gym/leap_year.rb
@@ -0,0 +1,14 @@
+years = [
+  1700,
+  1940,
+  2038
+]
+year = years.sample
+
+if (year % 4 == 0) && (year % 100 != 0)
+	pp "#{year} is a leap year!"
+elsif (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)
+	pp "#{year} is a leap year!"
+else
+	pp "#{year} is not a leap year."
+end
